bee52231870102fa034fd53fa651038d2c1f0f9c
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-07-23 00:17:53 -0400

    Update for discord.js 4

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import { REST } from "@discordjs/rest";
-import { Routes } from "discord-api-types/v9";
-import { Client, Collection, Intents } from "discord.js";
+import { Client, GatewayIntentBits, EmbedBuilder } from "discord.js";
 import * as wanikani from "./wanikani.js";
 import cron from "cron";
 import dotenv from "dotenv";
@@ -12,80 +10,42 @@ const WANIKANI_TOKENS = JSON.parse(process.env["WANIKANI_TOKENS"]);
 const TEST_GUILD_ID = process.env["TEST_GUILD_ID"];
 console.log(DISCORD_TOKEN, WANIKANI_TOKENS, TEST_GUILD_ID)
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const commands = [];
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.once('ready', () => {
-    const CLIENT_ID = client.user.id;
-    const rest = new REST({
-        version: '9'
-    }).setToken(DISCORD_TOKEN);
-    (async () => {
-        try {
-            if (!TEST_GUILD_ID) {
-                await rest.put(
-                    Routes.applicationCommands(CLIENT_ID), {
-                    body: commands
-                },
-                );
-                console.log('Successfully registered application commands globally');
-            } else {
-                await rest.put(
-                    Routes.applicationGuildCommands(CLIENT_ID, TEST_GUILD_ID), {
-                    body: commands
-                },
-                );
-                console.log('Successfully registered application commands for development guild');
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    })();
+async function sendMessage() {
+    const guild = client.guilds.cache.get(TEST_GUILD_ID);
+    const channel = guild.channels.cache.get("924956537787662336");
+    const standings = new EmbedBuilder();
+    standings.setTitle("WaniKani Standings: " + new Date().toLocaleDateString());
 
-    let scheduledMessage = new cron.CronJob('0 0 10,14,19,23 * * *', async () => {
-        const guild = client.guilds.cache.get(TEST_GUILD_ID);
-        const channel = guild.channels.cache.get("924956537787662336");
-        let standings = "";
-        for (let token of WANIKANI_TOKENS) {
-            let userStats = await wanikani.statsForDay(token);
-            
-            standings += `**${await wanikani.getName(token)}'s** stats for the day:\nReviews Completed: ${userStats["reviewsCompleted"]}\nReviews Remaining: ${userStats["reviewsPending"]}\nLessons Completed: ${userStats["lessonsCompleted"]}\nLessons Remaining: ${userStats["lessonsPending"]}\n\n`;
-        }
-        console.log(standings)
-        channel.send(standings);
-    });
-    (async () => {
-        const guild = client.guilds.cache.get(TEST_GUILD_ID);
-        const channel = guild.channels.cache.get("924956537787662336");
-        let standings = "";
-        for (let token of WANIKANI_TOKENS) {
-            let userStats = await wanikani.statsForDay(token);
-            standings += `**${await wanikani.getName(token)}'s** stats for the day:\nReviews Completed: ${userStats["reviewsCompleted"]}\nReviews Remaining: ${userStats["reviewsPending"]}\nLessons Completed: ${userStats["lessonsCompleted"]}\nLessons Remaining: ${userStats["lessonsPending"]}\n\n`;
-        }
-        console.log(standings)
-        channel.send(standings);
-    })();
+    let description = "";
+    for (const token of WANIKANI_TOKENS) {
+        let userStats = await wanikani.statsForDay(token);
+        const user = await wanikani.getUser(token);
+        description += `**[${user.username}'s](${user.profile_url})** [Level ${user.level}]:\n`;
+        description += `  -> Reviews Completed: ${userStats["reviewsCompleted"]}\n`;
+        description += `  -> Reviews Remaining: ${userStats["reviewsPending"]}\n`;
+        description += `  -> Lessons Completed: ${userStats["lessonsCompleted"]}\n`;
+        description += `  -> Lessons Remaining: ${userStats["lessonsPending"]}\n`;
+        description += "\n";
+    }
+    standings.setDescription(description);
+    channel.send({ embeds: [standings] });
+}
+
+client.once('ready', async () => {
+    let scheduledMessage = new cron.CronJob('0 0 12/2 * * *', () => sendMessage());
+    sendMessage();
+    
+    // for (const token of WANIKANI_TOKENS) {
+    //     await wanikani.statsForDay(token);
+    // }
     // When you want to start it, use:
     scheduledMessage.start()
-});
-
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await command.execute(client, interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true });
-    }
 });
 
 client.login(DISCORD_TOKEN);
--- wanikani.js
+++ wanikani.js
@@ -1,15 +1,32 @@
-import fetch from 'node-fetch';
-
-export const getName = async (token) => {
+export async function getUser(token) {
     let jsonResult = await fetch("https://api.wanikani.com/v2/user", {
         headers: { "Authorization": "Bearer " + token }
     }).then(response => response.json());
-    return jsonResult["data"]["username"];
+    return jsonResult.data;
 };
+
+export async function getSubjects(token) {
+    const subjects = {};
+    let next_url = "https://api.wanikani.com/v2/subjects";
+    while (next_url) {
+        let fetched_subjects = await fetch(next_url, {
+            headers: { "Authorization": "Bearer " + token }
+        }).then(response => response.json());
+        // console.log(fetched_subjects)
+        for (const subject of fetched_subjects.data) {
+            subjects[subject.id] = subject.data;
+        }
+        next_url = fetched_subjects.pages.next_url;
+    }
+    return subjects;
+}
 
 export const statsForDay = async (token) => {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
+
+    // const user = await getUser(token);
+    // const subjects = await getSubjects(token);
     
     let reviewsCompleted = (await fetch(`https://api.wanikani.com/v2/reviews?updated_after=${today.toISOString()}`, {
         headers: { "Authorization": "Bearer " + token }
@@ -26,12 +43,25 @@ export const statsForDay = async (token) => {
     let lessonsPending = (await fetch(`https://api.wanikani.com/v2/assignments?immediately_available_for_lessons`, {
         headers: { "Authorization": "Bearer " + token }
     }).then(response => response.json()))["total_count"];
-    
+    // let levelProgress = 0;
+    // const assignments = await fetch(`https://api.wanikani.com/v2/assignments`, {
+    //     headers: { "Authorization": "Bearer " + token }
+    // }).then(response => response.json());
+
+    // for (const assignment of assignments.data) {
+    //     console.log(subjects[assignment.data.subject_id].level)
+    // }
+
+    // const assignments_for_current_level = assignments.data.filter(assignment => subjects[assignment.data.subject_id].level == user.level);
+    // console.log("Yo")
+
+    // console.log(assignments_for_current_level)
+
     return {    
-        "reviewsCompleted": reviewsCompleted,
-        "reviewsPending": reviewsPending,
-        "lessonsCompleted": lessonsCompleted,
-        "lessonsPending": lessonsPending
+        reviewsCompleted,
+        reviewsPending,
+        lessonsCompleted,
+        lessonsPending,
     };
 }
 
